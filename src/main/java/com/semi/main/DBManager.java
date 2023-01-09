9abeb2c02e364b4662ab1ba5259c76ae7cb95ddc
kn1998 <kn1998pk@naver.com>
2023-01-09 14:52:34 +0900

    새 프로젝트 적용 및 회원 정보 수정에서 멈춤

--- src/main/java/com/semi/main/DBManager.java
+++ src/main/java/com/semi/main/DBManager.java
@@ -11,12 +11,12 @@ public class DBManager {
 	public static Connection connect() throws SQLException {
 	
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		return DriverManager.getConnection(url, "c##ljh1004", "ljh1004");
+		return DriverManager.getConnection(url, "c##pkr", "pkr");
 		
 		//재현 c##ljh1004 ljh1004
 		//우영
 		//재호
-		//경록
+		//경록c##pkr pkr
 		//승명
 
 	}
