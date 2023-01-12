512c3b2cf491e04b503369bb8840d68b2df53500
kn1998 <kn1998pk@naver.com>
2023-01-12 14:49:07 +0900

    reg, 수정, login css 만지기, 중복체크기능 진행

--- src/main/webapp/js/auth/acValueCheck.js
+++ src/main/webapp/js/auth/acValueCheck.js
@@ -4,7 +4,16 @@ function call() {
   const pw = document.querySelector("#pw");
   const pwConfirm = document.querySelector("#pw-confirm");
   const img = document.querySelector("#file");
+
   
+	if (isEmpty(id)) {
+    alert("아이디를 입력해 주세요!");
+    return false;
+  }
+	if (isEmpty(pw)) {
+    alert("비밀번호를 입력해 주세요!");
+    return false;
+  }
 	if (isEmpty(img)) {
     alert("사진을 입력해 주세요!");
     return false;
@@ -39,3 +48,23 @@ function call() {
     return false;
   }
 }
+
+function winopen(){
+	//새창을 열어서 페이지를 오픈 후 -> 회원아이디정보를 가지고 중복체크
+	//1. 아이디가 없으면 알림창과 진행x
+	if(document.fr.id.value =="" || document.fr.id.value.length < 0){
+		alert("아이디를 먼저 입력해주세요")
+		document.fr.id.focus();
+	}else{
+		//2. 회원정보아이디를 가지고 있는 지 체크하려면 DB에 접근해야한다.
+		//자바스크립트로 어떻게 DB에 접근할까? => 파라미터로 id값을 가져가서 jsp페이지에서 진행하면 된다.
+		window.open("joinIdCheck.jsp?userid="+document.fr.id.value,"","width=500, height=300");
+	}
+	
+}
+
+
+
+
+
+
