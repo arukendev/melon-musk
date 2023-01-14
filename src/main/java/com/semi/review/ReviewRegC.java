dd14448157051729c64ddf876db108e5291b3ba9
0hjho <48694062+0hjho@users.noreply.github.com>
2023-01-14 20:24:25 +0900

    페이지에 장기간 머물러서 세션 만료됐는데도 글쓰기, 삭제, 수정 버튼 작동하는 점 수정. 로그인페이지로 이동시킴.

--- src/main/java/com/semi/review/ReviewRegC.java
+++ src/main/java/com/semi/review/ReviewRegC.java
@@ -17,9 +17,15 @@ public class ReviewRegC extends HttpServlet {
 		AuthDAO.loginCheck(request);
 		HttpSession hs = request.getSession();
 		Auth a =(Auth)hs.getAttribute("account");
+		if(a==null) {
+			request.setAttribute("alert", "세션이 만료되었습니다. 재로그인 해주세요.");
+			request.setAttribute("contentPage", "jsp/auth/login.jsp");
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+		} else {
 		request.setAttribute("a", a);
 		request.setAttribute("contentPage", "jsp/review/review_reg.jsp");
 		request.getRequestDispatcher("index.jsp").forward(request, response);
+		}
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/java/com/semi/review/ReviewUpdateC.java
+++ src/main/java/com/semi/review/ReviewUpdateC.java
@@ -14,12 +14,19 @@ import com.semi.auth.AuthDAO;
 @WebServlet("/ReviewUpdateC")
 public class ReviewUpdateC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		AuthDAO.loginCheck(request);		
+		AuthDAO.loginCheck(request);
+		HttpSession hs = request.getSession();
+		Auth a =(Auth)hs.getAttribute("account");
+		if(a==null) {
+			request.setAttribute("alert", "세션이 만료되었습니다. 재로그인 해주세요.");
+			request.setAttribute("contentPage", "jsp/auth/login.jsp");
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+		} else {
 		ReviewDAO.getReview3(request);
 		
 		request.setAttribute("contentPage", "jsp/review/review_update.jsp");
 		request.getRequestDispatcher("index.jsp").forward(request, response);
-		
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
