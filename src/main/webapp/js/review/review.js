e854196f127b42d152a893a52fb98d56c4a76d5a
0hjho <48694062+0hjho@users.noreply.github.com>
2023-01-15 00:53:59 +0900

    수정, 삭제시 로그인도 요구하도록

--- src/main/webapp/js/review/review.js
+++ src/main/webapp/js/review/review.js
@@ -25,20 +25,30 @@ function like(no, writer, au_id) {
 }
 
 function update(no, writer, au_id) {
-	if(au_id!=writer){
-		alert('본인 글만 수정할 수 있습니다.');
-	} else{
-		location.href="ReviewUpdateC?no="+no;
+	if(au_id==""){
+		alert('리뷰를 수정하려면 로그인 해주세요.');
+		location.href="ReviewLikeC";
+	} else {
+		if(au_id!=writer){
+			alert('본인 글만 수정할 수 있습니다.');
+		} else{
+			location.href="ReviewUpdateC?no="+no;
+		}
 	}
 }
 
 function deleteReview(no, writer, au_id) {
-	if(au_id!=writer){
-		alert('본인 글만 삭제할 수 있습니다.');
-	} else{
-		const really = confirm('정말 삭제하겠습니까?');
-		if(really){
-		location.href="ReviewDelC?no="+no;
+	if(au_id==""){
+		alert('리뷰를 삭제하려면 로그인 해주세요.');
+		location.href="LoginC";
+	} else {
+		if(au_id!=writer){
+			alert('본인 글만 삭제할 수 있습니다.');
+		} else{
+			const really = confirm('정말 삭제하겠습니까?');
+			if(really){
+			location.href="ReviewDelC?no="+no;
+			}
 		}
 	}
 }
