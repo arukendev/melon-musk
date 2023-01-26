fab48b06979f0395347155669051e1663a8a6504
aruken <iamjaehyun4@gmail.com>
2023-01-27 00:14:08 +0900

    아무튼 일단 다함

--- src/main/java/com/semi/home/MyMusicLike.java
+++ src/main/java/com/semi/home/MyMusicLike.java
@@ -3,14 +3,18 @@ package com.semi.home;
 public class MyMusicLike {
 	private String id;
 	private String name;
+	private String img;
+	private String artist;
 	public MyMusicLike() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public MyMusicLike(String id, String name) {
+	public MyMusicLike(String id, String name, String img, String artist) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.img = img;
+		this.artist = artist;
 	}
 	public String getId() {
 		return id;
@@ -23,6 +27,18 @@ public class MyMusicLike {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
 	}
 	
 	
